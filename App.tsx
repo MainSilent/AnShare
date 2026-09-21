a88264be6091fb3902c4cc2fad7b572c0f65b436
MainSilent <70273681+MainSilent@users.noreply.github.com>
2026-09-21 23:28:18 +0330

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,45 +1,75 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
+import React, { useEffect } from 'react';
 import {
-  SafeAreaProvider,
-  useSafeAreaInsets,
-} from 'react-native-safe-area-context';
+  StatusBar,
+  useColorScheme,
+  StyleSheet,
+  TouchableOpacity,
+  Text,
+  Linking
+} from 'react-native';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
+
 
 function App() {
-  const isDarkMode = useColorScheme() === 'dark';
+  const isDarkMode = useColorScheme() === 'dark'
 
   return (
-    <SafeAreaProvider>
+    <SafeAreaProvider style={styles.container}>
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <AppContent />
+
+      <TouchableOpacity
+        // onPress={() => connect()}
+        style={{
+          ...styles.button,
+          // backgroundColor: server == 0 ? "green" : server == 1 ? "#bab72e" : "red"
+        }}
+      >
+        <Text style={styles.buttonText}>
+          {/* {server == 0 ? "Connect" : server == 1 ? "Wait..." : "Disconnect"} */}
+        </Text>
+      </TouchableOpacity>
+
+      {/* <Text style={{...styles.ipText, marginTop: 40}}>Port: {PORT}</Text> */}
+      {/* <Text style={styles.ipText}>IP: {isWifi ? details.ipAddress : "XXX.XXX.XXX.XXX"}</Text> */}
+
+      {/* {isWifi && server == 2 &&
+        <TouchableOpacity onPress={() => Linking.openURL(url)}>
+          <Text style={{...styles.urlText}}>{url}</Text>
+        </TouchableOpacity>
+      } */}
     </SafeAreaProvider>
-  );
-}
-
-function AppContent() {
-  const safeAreaInsets = useSafeAreaInsets();
-
-  return (
-    <View style={styles.container}>
-      <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
-      />
-    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: '#12111c',
+    alignItems: 'center',
+    justifyContent: 'center'
   },
-});
+  button: {
+    padding: 20,
+    paddingHorizontal: 40,
+    borderRadius: 10,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 18
+  },
+  ipText: {
+    marginTop: 15,
+    color: 'white',
+    fontSize: 18
+  },
+  urlText: {
+    fontSize: 20,
+    marginTop: 30,
+    color: 'lightblue',
+    textDecorationLine: 'underline'
+  }
+})
 
-export default App;
+export default App
