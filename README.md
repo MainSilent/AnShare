26c87417d859238ab0db116683d61b6d659da777
MainSilent <70273681+MainSilent@users.noreply.github.com>
2026-09-21 19:27:28 +0330

    upgrade

--- README.md
+++
@@ -1,3 +0,0 @@
-# AN Share
-
-### In Progress...
--- android/app/src/main/java/com/anshare/MainActivity.kt
+++ android/app/src/main/java/com/anshare/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.an_share
+package com.anshare
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
@@ -11,7 +11,7 @@ class MainActivity : ReactActivity() {
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
-  override fun getMainComponentName(): String = "an_share"
+  override fun getMainComponentName(): String = "AnShare"
 
   /**
    * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
