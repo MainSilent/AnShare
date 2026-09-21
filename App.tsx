ddc92eab756bd587a4ffe0dd677304158f222466
MainSilent <70273681+MainSilent@users.noreply.github.com>
2026-09-21 23:30:56 +0330

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import {
   StatusBar,
   useColorScheme,
@@ -9,34 +9,38 @@ import {
 } from 'react-native';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 
+let host
+const PORT = 9630
 
 function App() {
-  const isDarkMode = useColorScheme() === 'dark'
+  const [url, setURL] = useState('')
+  const [server, setServer] = useState(0)
+  const [ isWifi, setWifi ] = useState(false)
 
   return (
     <SafeAreaProvider style={styles.container}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <StatusBar barStyle={'dark-content'} />
 
       <TouchableOpacity
         // onPress={() => connect()}
         style={{
           ...styles.button,
-          // backgroundColor: server == 0 ? "green" : server == 1 ? "#bab72e" : "red"
+          backgroundColor: server == 0 ? "green" : server == 1 ? "#bab72e" : "red"
         }}
       >
         <Text style={styles.buttonText}>
-          {/* {server == 0 ? "Connect" : server == 1 ? "Wait..." : "Disconnect"} */}
+          {server == 0 ? "Connect" : server == 1 ? "Wait..." : "Disconnect"}
         </Text>
       </TouchableOpacity>
 
-      {/* <Text style={{...styles.ipText, marginTop: 40}}>Port: {PORT}</Text> */}
-      {/* <Text style={styles.ipText}>IP: {isWifi ? details.ipAddress : "XXX.XXX.XXX.XXX"}</Text> */}
+      <Text style={{...styles.ipText, marginTop: 40}}>Port: {PORT}</Text>
+      <Text style={styles.ipText}>IP: {isWifi ? details.ipAddress : "XXX.XXX.XXX.XXX"}</Text>
 
-      {/* {isWifi && server == 2 &&
+      {isWifi && server == 2 &&
         <TouchableOpacity onPress={() => Linking.openURL(url)}>
           <Text style={{...styles.urlText}}>{url}</Text>
         </TouchableOpacity>
-      } */}
+      }
     </SafeAreaProvider>
   );
 }
